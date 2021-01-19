6a330bf177f3884049f632e76681eba53c9a86f6
Marvin Dostal <maffinmaffinmaffinmaffin@gmail.com>
2021-01-19 15:41:23 +0100

    Decrease boldness and add current year to copyright

--- script_helper/generate_bold_sfd.sh
+++ script_helper/generate_bold_sfd.sh
@@ -6,12 +6,12 @@ font = fontforge.open("src/mononoki-Regular.sfd")
 font.fontname = 'MononokiFork-Bold'
 font.fullname = 'Mononoki Fork Bold'
 font.weight = 'Bold'
-font.private['StemSnapH'] = [102]
-font.private['StemSnapV'] = [110]
+font.private['StemSnapH'] = [97]
+font.private['StemSnapV'] = [105]
 font.os2_weight = 700
 
 font.selection.all() # TODO: exclude box drawing characters, etc.
-font.changeWeight(25, "auto", 0, 0, "squish")
+font.changeWeight(20, "auto", 0, 0, "squish")
 
 font.save("src/mononoki-Bold.sfd")
 
@@ -23,12 +23,12 @@ font = fontforge.open("src/mononoki-Italic.sfd")
 font.fontname = 'MononokiFork-Bold-Italic'
 font.fullname = 'Mononoki Fork Bold Italic'
 font.weight = 'Bold'
-font.private['StemSnapH'] = [102]
-font.private['StemSnapV'] = [110]
+font.private['StemSnapH'] = [97]
+font.private['StemSnapV'] = [105]
 font.os2_weight = 700
 
 font.selection.all() # TODO: exclude box drawing characters, etc.
-font.changeWeight(25, "auto", 0, 0, "squish")
+font.changeWeight(20, "auto", 0, 0, "squish")
 
 font.save("src/mononoki-BoldItalic.sfd")
 
