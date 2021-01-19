a62227d27fe3ee9d5d3d62abca87fedf6e3d098d
Marvin Dostal <maffinmaffinmaffinmaffin@gmail.com>
2021-01-19 14:16:08 +0100

    [WIP] Generate the bold variants

--- script_helper/generate_bold_sfd.sh
+++ script_helper/generate_bold_sfd.sh
@@ -3,13 +3,33 @@
 fontforge - << END
 font = fontforge.open("src/mononoki-Regular.sfd")
 
-font.fontname = 'ComicMono-Bold'
-font.fullname = 'Comic Mono Bold'
+font.fontname = 'MononokiFork-Bold'
+font.fullname = 'Mononoki Fork Bold'
 font.weight = 'Bold'
+font.private['StemSnapH'] = [98]
+font.private['StemSnapV'] = [105]
+font.os2_weight = 700
 
-font.selection.all() # TODO exclude box drawing characters, etc.
-font.changeWeight(25, "LCG", 0, 0, "squish")
+font.selection.all() # TODO: exclude box drawing characters, etc.
+font.changeWeight(20, "auto", 0, 0, "squish")
 
 font.save("src/mononoki-Bold.sfd")
+
+END
+
+fontforge - << END
+font = fontforge.open("src/mononoki-Italic.sfd")
+
+font.fontname = 'MononokiFork-BoldItalic'
+font.fullname = 'Mononoki Fork BoldItalic'
+font.weight = 'Bold'
+font.private['StemSnapH'] = [98]
+font.private['StemSnapV'] = [105]
+font.os2_weight = 700
+
+font.selection.all() # TODO: exclude box drawing characters, etc.
+font.changeWeight(20, "auto", 0, 0, "squish")
+
+font.save("src/mononoki-BoldItalic.sfd")
 
 END
