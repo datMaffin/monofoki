cdd7d071179dd94c5c54a6ebda414f3d93f9d922
Marvin Dostal <maffinmaffinmaffinmaffin@gmail.com>
2021-09-25 20:25:26 +0200

    Add `shell.nix` specifying dev tools, normalize sfd files and generate new specimen

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let 
+  sfdnormalize = pkgs.python39Packages.buildPythonPackage rec {
+    pname = "sfdnormalize";
+    version = "1.x.x";
+
+    src = fetchTarball {
+      url = "https://github.com/alerque/sfdnormalize/tarball/905284d04cbe651d6f5e08075c01198281d7d3f3";
+      sha256 = "1qsyhsxckmphrx5alyg964ypg91waij7drddv3ybqbsvy6pqmivp";
+    };
+  };
+in
+  pkgs.mkShell {
+    nativeBuildInputs = [
+      pkgs.python39
+      sfdnormalize
+      pkgs.fontforge
+      pkgs.ttfautohint
+    ];
+  }
